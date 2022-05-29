3d3a77c9d1919f767ee1c1c3c2b3fc612f0f53ee
MaciejDubowik <maciejxpoland@gmail.com>
2022-05-29 13:39:59 +0200

    storyboard sketch

---
+++ PESEL-Validator/PESEL-Validator/model/Pesel.swift
@@ -0,0 +1,16 @@
+//
+//  Pesel.swift
+//  PESEL-Validator
+//
+//  Created by Maciej Dubowik on 29/05/2022.
+//
+
+import UIKit
+
+struct Pesel{
+    let number: Int
+    let message: String
+    let gender: Bool
+    let dateOfBirth: Date
+    let color: UIColor
+}
---
+++ PESEL-Validator/PESEL-Validator/model/Validator.swift
@@ -0,0 +1,24 @@
+//
+//  Validator.swift
+//  PESEL-Validator
+//
+//  Created by Maciej Dubowik on 29/05/2022.
+//
+
+import UIKit
+
+struct Validator {
+    var pesel: Pesel?
+    
+    func getGender() -> String{
+    return ""
+    }
+    
+    func getDateOfBirth() -> String{
+    return ""
+    }
+    
+    func calculateAge() -> Int{
+    return 0
+    }
+}
