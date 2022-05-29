42713db463450600dc730338a8ce446ca0ebfde3
MaciejDubowik <maciejxpoland@gmail.com>
2022-05-29 11:12:54 +0200

    initital commit

---
+++ PESEL-Validator/PESEL-Validator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PESEL-Validator
+//
+//  Created by Maciej Dubowik on 29/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
